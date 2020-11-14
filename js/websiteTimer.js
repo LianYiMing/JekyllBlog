c3a20f603df1dc8973079e2c64b0465ac56627dd
廉一鸣 <AGZX_lym@163.com>
2020-11-14 23:00:12 +0800

    破壳了！！！

---
+++ js/websiteTimer.js
@@ -0,0 +1,45 @@
+//
+function lianyiming() {
+var now = new Date
+var bornyear = "2020"
+var bornmonth = "11"
+var borndate = "7"
+var bornhour = "7"
+var bornminute = "30"
+var bornsecond = "0"
+var nowyear = now.getFullYear()
+var nowmonth = now.getMonth() + 1
+var nowdate = now.getDate()
+var nowhour = now.getHours()
+var nowminute = now.getMinutes()
+var nowsecond = now.getSeconds()
+if(bornmonth > nowmonth){
+	var month = bornmonth - nowmonth
+}else{  var month = nowmonth - bornmonth}
+
+
+if(bornhour > nowhour){
+		var hour = bornhour - nowhour
+	}else{  var hour = nowhour - bornhour}
+
+if(borndate > nowdate){
+		var date = borndate - nowdate
+	}else{  var date = nowdate - borndate}
+
+if(bornminute> nowminute){
+	var minute = bornminute -nowminute
+}else{
+	var minute =  nowminute - bornminute
+}
+var second = nowsecond
+var year = nowyear - bornyear
+
+var time =  year + "年" + month + "月" + date +"天" + hour +  "时" + minute + "分"   +  second +"秒"
+if ( year == 0 ) {
+	var time =  month + "月" + date +"天" + hour +  "时" + minute + "分"   +  second +"秒"
+}
+
+document.getElementById("timer").innerHTML = time ;
+console.log("廉一鸣好帅")
+}
+setInterval(lianyiming,1000);
